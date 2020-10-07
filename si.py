de31fa51888c47df88cbab705236e2a6032a1c51
Jakub Hryc <jakubrhryc@gmail.com>
2020-10-07 19:09:49 +0200

    Add some private methods

--- si.py
+++ si.py
@@ -2,12 +2,14 @@
 
 class Robot(object):
     total = 0
-    def __init__(self, name, height, weight):
+    def __init__(self, name, height, weight, mood):
         print("Powstal nowy robot");
         self.name = name
         self.height = height
         self.weight = weight
+        self.__mood = mood
         Robot.total += 1
+
 
     def __str__(self):
         rep = "Obiekt klasy Robot \n"
@@ -15,20 +17,25 @@ class Robot(object):
         return rep
     def przelicznik(self):
         lbs = 2.2046 * self.weight
-        print("Waze ", lbs, " funtow")
+        return lbs
     def talk(self):
-        print("Czesc! Jestem", self.name, "\n" + "Waze ", self.weight, "kilogramów")
+        print("Czesc! Jestem", self.name, "\n" + "Waze ", self.weight, "kilogramów, czyli tyle funtów", self.przelicznik())
+        print("Mój nastrój określam jako: ", self.__mood,"\n")
+        self.__sekret()
+
 
     @staticmethod
     def status():
         print("\nOgolna liczba robotow wynosi ", Robot.total)
 
+    def __sekret(self):
+        print("Sekretem każdego robota jest to, ze nie ma zadnego sekretu.")
 #Czesc glowna kodu
-sophia = Robot("Sophia",160,55)
+sophia = Robot("Sophia",160,55, "smutny")
 sophia.talk()
 sophia.przelicznik()
 
-alice = Robot("Alice",150,50)
+alice = Robot("Alice",150,50, "zadowolony")
 alice.talk()
 print("Wyświetlenie obiektu sophia: \n" + str(sophia) + "\n")
 print("Wyświetlenie nazwy obiektu sophia: \n" + sophia.name)
